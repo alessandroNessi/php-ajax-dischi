7e0fc486e1ab51042bf2b8b4fb17f18fc41c4dc2
alessandroNessi <alessandronessi@msn.com>
2021-10-21 16:59:01 +0200

    populated and uppercase

--- components/card.php
+++ components/card.php
@@ -1,8 +1,9 @@
 <?php 
     function writeCard($title,$author,$year,$poster){
+        $upperCase="strtoupper";
         echo "<div class='card_container'><div class='card'>
             <a href='https://en.wikipedia.org/wiki/{$author}'><img src='{$poster}' alt='{$title}'></a>
-            <h2 class='title'>{$title}</h2>
+            <h2 class='title'>{$upperCase($title)}</h2>
             <p class='subtitle'>{$author}</p>
             <p class='subtitle'>{$year}</p>
         </div></div>";
--- data/data.php
+++ data/data.php
@@ -35,6 +35,30 @@ $database = [
 		'author' => 'Eric Clapton',
 		'year' => 2002,
 		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+	],
+	[
+		'title' => 'Made In Japan',
+		'author' => 'Deep Purple',
+		'year' => 1972,
+		'poster' => 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c5/Deep_Purple_Made_in_Japan.jpg/220px-Deep_Purple_Made_in_Japan.jpg'
+	],
+	[
+		'title' => 'And Justice For All',
+		'author' => 'Metallica',
+		'year' => 1988,
+		'poster' => 'https://img.discogs.com/vloL5f2_ibJ2nMc3w6BKFSnnkfk=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-371162-1531515576-1093.jpeg.jpg'
+	],
+	[
+		'title' => 'Hard Wired',
+		'author' => 'David Weckl',
+		'year' => 1994,
+		'poster' => 'https://images-na.ssl-images-amazon.com/images/I/611TBSi2y8L._AC_SX679_.jpg'
+	],
+	[
+		'title' => 'Bad',
+		'author' => 'Michael Jackson',
+		'year' => 1987,
+		'poster' => 'https://upload.wikimedia.org/wikipedia/en/5/51/Michael_Jackson_-_Bad.png'
 	]
 ];
 ?>
