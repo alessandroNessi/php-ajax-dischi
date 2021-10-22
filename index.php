f563346e95f2a2e64fe8ede494652d5f6450ab33
alessandroNessi <alessandronessi@msn.com>
2021-10-22 15:21:40 +0200

    chiamata axios

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-    include __DIR__.("/data/data.php");
-    // include __DIR__.("/components/header.php");
-    include __DIR__.("/components/card.php");
-    // var_dump($database);
+    include __DIR__.("/data/database.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,9 +18,9 @@
         <img src="https://png2.cleanpng.com/sh/fdb987b25544360cc0ccddf830309d3d/L0KzQYq3VsA5N6J0hJH0aYP2gLBuTfxwb5CyiAJ4dHnpiX73jCJ1aZNxfZ93ZYT6f8PyTfdzaaFtgdV8LYP3grbojflvb15yRdN1dHX1frL7igZmNZpzjtd8dH3ofsX6TgJ2fKVqhtRucnewd7F5hP9vNZpzjtd8dD24c7fqVfY3bWE7SqdvNz6zQ4O8UMA6OmI6TqI5NEO2R4W3UsUzNqFzf3==/kisspng-logo-spotify-portable-network-graphics-streaming-m-alternative-investments-ruttenberg-gordon-invest-5cfc5f6e0625f7.0325009215600433740252.png" alt="spotify logo">
     </header>
     <main>
-        <div class="container">
+        <div id="app" class="container">
             <?php foreach ($database as $data){ ?>
-                <div class='card_container'><div class='card'>
+                <div v-for((element, index) in data) class='card_container'><div class='card'>
                     <a href='https://en.wikipedia.org/wiki/<?php echo $data['author']?>'><img src='<?php echo $data['poster']?>' alt='<?php echo $data['title']?>'></a>
                     <h2 class='title'><?php echo $data['title']?></h2>
                     <p class='subtitle'><?php echo $data['author']?></p>
@@ -34,5 +31,11 @@
     </main>
     <img src="" alt="">
     <footer></footer>
+    <!-- includo vue -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <!-- includo axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- includo il mio script che userò per la chiamata axios -->
+    <script src="scripts/script.js"></script>
 </body>
 </html>
